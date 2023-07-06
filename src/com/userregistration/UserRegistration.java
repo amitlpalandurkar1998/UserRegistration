b5f77466eeddddc062507fcd901b8f75e8242d7b
Amit <amitlpalandurkar1998@gmail.com>
2023-07-06 16:23:43 +0530

    check User First Name is Valid Or Not.

--- src/com/userregistration/UserRegistration.java
+++ src/com/userregistration/UserRegistration.java
@@ -1,4 +1,21 @@
 package com.userregistration;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+    Scanner scanner = new Scanner(System.in);
+    void getUserFirstName(){
+        Pattern pattern = Pattern.compile("[A-Z]+[a-zA-Z]{2,}[0-9]*$");
+        System.out.println("Enter The First Name : ");
+        String firstName = scanner.nextLine();
+
+        Matcher matcher = pattern.matcher(firstName);
+        if (matcher.matches()){
+            System.out.println("First Name Is Valid.");
+        }else {
+            System.out.println("First Name Is InValid.");
+        }
+    }
 }
