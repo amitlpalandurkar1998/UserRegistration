3416a6bcba5f7c7c217492dbe2018a885d0c9928
Amit <amitlpalandurkar1998@gmail.com>
2023-07-08 15:31:56 +0530

    Adding Email ID Method and checking the validity.

--- src/com/userregistration/UserRegistration.java
+++ src/com/userregistration/UserRegistration.java
@@ -23,14 +23,29 @@ public class UserRegistration {
     void getUserLastName(){
         Pattern pattern = Pattern.compile("[A-Z]+[a-zA-Z]{2,}[0-9]*$");
         System.out.print("Enter The Last Name : ");
-        String firstName = scanner.nextLine();
+        String lastName = scanner.nextLine();
 
-        Matcher matcher = pattern.matcher(firstName);
+        Matcher matcher = pattern.matcher(lastName);
         if (matcher.matches()){
-            System.out.print("Last Name Is Valid.\n");
+            System.out.println("Last Name Is Valid.\n");
+            getUserEmailID();
         }else {
             System.out.println("Last Name Is InValid.");
             getUserLastName();
+        }
+    }
+    void getUserEmailID(){
+        Pattern pattern = Pattern.compile("[a-z0-9]{3,}.[a-z0-9]{3,}@{1}(abc|bl|co){1}.co.in");
+        System.out.println("Follow the format (abc123.xyz789@(abc or bl or co).co.in)");
+        System.out.print("Enter The Email ID : ");
+        String emailID  = scanner.nextLine();
+
+        Matcher matcher = pattern.matcher(emailID);
+        if (matcher.matches()){
+            System.out.print("Email ID Is Valid.\n");
+        }else {
+            System.out.println("Email ID Is InValid.");
+            getUserEmailID();
         }
     }
 }
